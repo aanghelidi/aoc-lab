48e0a3a94d01b0e1d0a1d1977f64cdf1170c0136
Alain Anghelidi <alainanghelidi@gmail.com>
2023-10-08 21:59:17 +0200

    feat: add part 1 day 7

--- 2017/day07/main.go
+++ 2017/day07/main.go
@@ -7,35 +7,66 @@ import (
 	"strings"
 )
 
-type Data struct {
-	weight    int
-	childrens []string
-}
-type Graph map[string]Data
-
 func ExtractNumber(nstr string) int {
 	cleaned := strings.TrimFunc(nstr, func(r rune) bool { return r == '(' || r == ')' })
 	number, _ := strconv.Atoi(cleaned)
 	return number
 }
 
+type Node struct {
+	name      string
+	childrens []*Node
+}
+
+type Tree []*Node
+
+func (t Tree) FindRoot() *Node {
+	nodeToParent := make(map[string]string, len(t))
+	for _, node := range t {
+		for _, child := range node.childrens {
+			nodeToParent[child.name] = node.name
+		}
+	}
+	for _, node := range t {
+		// if not is set -> no parent -> root
+		if _, isParent := nodeToParent[node.name]; !isParent {
+			return node
+		}
+	}
+	return nil
+}
+
 func main() {
-	data, _ := os.ReadFile("test.txt")
+	data, _ := os.ReadFile("input.txt")
 	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
-	graph := make(Graph, len(lines))
-	for _, line := range lines {
+	tree := make(Tree, len(lines))
+	weights := make(map[string]int64, len(lines))
+	// Build Tree (graph)
+	for i, line := range lines {
 		parts := strings.Split(line, " ")
 		nParts := len(parts)
 		if nParts > 2 {
 			name := parts[0]
 			weight := ExtractNumber(parts[1])
-			childrens := parts[3:nParts]
-			graph[name] = Data{weight, childrens}
+			weights[name] = int64(weight)
+			childrens := make([]*Node, nParts-3)
+			for j := 0; j < nParts-3; j++ {
+				child := strings.TrimSuffix(strings.TrimSpace(parts[j+3]), ",")
+				childNode := Node{name: child}
+				childrens[j] = &childNode
+			}
+			node := Node{name: name, childrens: childrens}
+			tree[i] = &node
 		} else {
 			name := parts[0]
 			weight := ExtractNumber(parts[1])
-			graph[name] = Data{weight, nil}
+			weights[name] = int64(weight)
+			node := Node{name: name}
+			tree[i] = &node
 		}
 	}
-	fmt.Printf("%v", graph)
+	// Find Root
+	root := tree.FindRoot()
+	fmt.Printf("Part 1: %s\n", root.name)
+	// Part 2 - Balance the Tree from root
 }
